1cfc9db3bafbde70fa7d5ab1a99cc56a8eebf51a
Laércio <laercioamedeiros@gmail.com>
2023-01-10 12:58:21 -0300

    User Services and Controllers are done

---
+++ src/services/users/createUser.service.ts
@@ -0,0 +1,32 @@
+import { IUserRequest } from "../../interfaces/users";
+import AppDataSource from "../../data-source";
+import { User } from "../../entities/user.entity";
+import { userWithoutPasswordSerializer } from "../../serializers/user.serializer";
+import { AppError } from "../../errors/AppError";
+
+const createUserService = async (userData: IUserRequest) => {
+  
+  const userRepository = AppDataSource.getRepository(User);
+
+  const userAlreadyExists = await userRepository.findOneBy({
+    email: userData.email,
+  });
+
+  if (userAlreadyExists) {
+    throw new AppError("User Already Exists", 409);
+  }
+
+  const newUser = userRepository.create(userData);
+
+  const newSavedUser = await userRepository.save(newUser);
+
+  const userWithoutPassword = await userWithoutPasswordSerializer.validate(
+    newSavedUser,
+    {
+      stripUnknown: true,
+    }
+  );
+  return userWithoutPassword;
+};
+
+export default createUserService;
---
+++ src/services/users/deleteUser.service.ts
@@ -0,0 +1,17 @@
+import { IUser, IUserRequest } from "../../interfaces/users";
+import AppDataSource from "../../data-source";
+import { User } from "../../entities/user.entity";
+import { userWithoutPasswordSerializer } from "../../serializers/user.serializer";
+import { AppError } from "../../errors/AppError";
+
+const userDeleteService = async (id: string): Promise<void> => {
+  const userRepository = AppDataSource.getRepository(User);
+
+  const findUser = await userRepository.findOneBy({
+    id: id,
+  });
+
+  await userRepository.softRemove(findUser);
+};
+
+export default userDeleteService;
