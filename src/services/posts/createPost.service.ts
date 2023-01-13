37a61a312ed82a03a564b3c4bdb4c2cfa9fdba08
bj-mandim <beatrizjuliomandim@gmail.com>
2023-01-13 18:32:24 -0300

    feat: Created post services

--- src/services/posts/createPost.service.ts
+++ src/services/posts/createPost.service.ts
@@ -1,2 +1,13 @@
-const createPostService = async () =>{}
-export default createPostService 
+import Post from "../../entities/posts.entity";
+import { IPostRequest, IPost } from "../../interfaces/posts.interface";
+import AppDataSource from "../../data-source";
+
+const createPostService = async (postData: IPost): Promise<Post> => {
+  const postRepository = AppDataSource.getRepository(Post);
+
+  const post = postRepository.create(postData);
+  await postRepository.save(post);
+
+  return post;
+};
+export default createPostService;
--- src/services/posts/deletePost.service.ts
+++ src/services/posts/deletePost.service.ts
@@ -1,2 +1,19 @@
-const deletePostService = async ()  =>{}
-export default deletePostService
+import Post from "../../entities/posts.entity";
+import AppDataSource from "../../data-source";
+import { AppError } from "../../errors/AppError";
+import { IPost } from "../../interfaces/posts.interface";
+
+const deletePostService = async (id: number): Promise<void> => {
+  const PostRepository = AppDataSource.getRepository(Post);
+
+  const findPost: IPost = await PostRepository.findOneBy({
+    id: Number(id),
+  });
+
+  if (findPost == undefined) {
+    throw new AppError("Post does not exists", 400);
+  }
+
+  await PostRepository.delete(findPost);
+};
+export default deletePostService;
